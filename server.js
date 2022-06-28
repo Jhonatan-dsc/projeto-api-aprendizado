698077f1f3d0e9e0407ef590e93dedd539ddcc20
Jhones <88455271+Jhonatan-dsc@users.noreply.github.com>
2022-06-28 18:59:44 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+//inicio api
+const express = require ('express');
+
+//
+const cors = require ('cors');
+   
+//
+const server = express();
+server.use(express.json());
+//
+
+//
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) =>{
+    
+    return res.json([ 
+      
+        //    
+        { Motorista: 'João Silva'},
+        { carro:'fiat agro', cor:'azul', placa: 'XYZ01'},
+        //
+        {data_de_inicio: ' 25/12/2022 às 13:00'},
+        {data_de_termino: '25/12/2022 às 18:00'},
+        //
+        {motorista_que_utilizou: 'João Silva'},
+        {automovel_utilizado:'Fiat Agro'}
+
+    ])
+    
+    
+})
+app.listen(4567)
